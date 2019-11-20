0009ee5d0a5ceefabe99e74d614ec94e194eabee
SNoof85 <snoof85@gmail.com>
2019-11-20 18:51:48 +0100

    Update sensor.py

--- freebox/sensor.py
+++ freebox/sensor.py
@@ -11,8 +11,40 @@ _LOGGER = logging.getLogger(__name__)
 async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
     """Set up the sensors."""
     fbx = hass.data[DATA_FREEBOX]
-    async_add_entities([FbxRXSensor(fbx), FbxTXSensor(fbx), FbxCpuBTemp(fbx), FbxCpuMTemp(fbx), FbxSwitchTemp(fbx)], True)
+    fbx = hass.data[DATA_FREEBOX]
+    sys_config = await fbx.system.get_config()
+    for sensor_id in sys_config['sensors']:
+        async_add_entities([FbxTempSensor(fbx, sensor_id['id'])], True)
+    async_add_entities([FbxRXSensor(fbx), FbxTXSensor(fbx)], True)
 
+
+class FbxTempSensor(Entity):
+
+    def __init__(self, fbx, sensor_id):
+        self._fbx = fbx
+        self._datas = None
+        self._state = None
+        self._sensor_id = sensor_id
+        self._unit_of_measurement = "°C"
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def state(self):
+        return self._state
+
+    @property
+    def unit_of_measurement(self):
+        return self._unit_of_measurement
+
+    async def async_update(self):
+        self._datas = await self._fbx.system.get_config()
+        for temp_sensor in self._datas['sensors']:
+            if temp_sensor['id'] == self._sensor_id:
+                self._name = temp_sensor['name']
+                self._state = temp_sensor['value']
 
 class FbxConnSensor(Entity):
     """Representation of a freebox sensor."""
@@ -39,87 +71,6 @@ class FbxConnSensor(Entity):
         """Fetch status from freebox."""
         self._datas = await self._fbx.connection.get_status()
 
-class FbxSysSensor(Entity):
-    """Representation of a freebox system sensor."""
-
-    _name = "generic"
-
-    def __init__(self, fbx):
-        """Initialize the sensor."""
-        self._fbx = fbx
-        self._state = None
-        self._datas = None
-
-    @property
-    def name(self):
-        """Return the name of the sensor."""
-        return self._name
-
-    @property
-    def device_class(self):
-        """Returns the device class."""
-        return self._device_class
-
-    @property
-    def unit_of_measurement(self):
-        """Returns unit of measeurement."""
-        return self._unit_of_measurement
-
-    @property
-    def state(self):
-        """Return the state of the sensor."""
-        return self._state
-
-    async def async_update(self):
-        """Fetch status from freebox."""
-        self._datas = await self._fbx.system.get_config()
-
-class FbxCpuBTemp(FbxSysSensor):
-    """Update the system sensor."""
-    _name = "Freebox CPUB Temperature"
-    _device_class = "temperature"
-    _unit_of_measurement = "°C"
-    
-    async def async_update(self):
-        """Get value from fetched datas."""
-        await super().async_update()
-        if self._datas is not None:
-            self._state = self._datas["temp_cpub"]
-
-class FbxCpuMTemp(FbxSysSensor):
-    """Update the system sensor."""
-    _name = "Freebox CPUM Temperature"
-    _device_class = "temperature"
-    _unit_of_measurement = "°C"
-    
-    async def async_update(self):
-        """Get value from fetched datas."""
-        await super().async_update()
-        if self._datas is not None:
-            self._state = self._datas["temp_cpum"]
-
-class FbxSwitchTemp(FbxSysSensor):
-    """Update the system sensor."""
-    _name = "Freebox switch Temperature"
-    _device_class = "temperature"
-    _unit_of_measurement = "°C"
-    
-    async def async_update(self):
-        """Get value from fetched datas."""
-        await super().async_update()
-        if self._datas is not None:
-            self._state = self._datas["temp_sw"]
-
-class FbxConnState(FbxConnSensor):
-    """Update The connection status sensor."""
-    
-    _name = "Freebox connection status"
-    
-    async def async_update(self):
-        """Get the value from fetched datas."""
-        await super().async_update()
-        if self._datas is not None:
-            self._state = self._datas["state"]
 
 class FbxRXSensor(FbxConnSensor):
     """Update the Freebox RxSensor."""
